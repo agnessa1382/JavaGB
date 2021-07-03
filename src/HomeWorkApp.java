23c85b2f18a4fd1906355d09a95683498afec5be
Agnessa Sherieva <agnessa1382@mail.ru>
2021-07-03 18:03:51 +0300

    Revert "ДЗ №3"
    This reverts commit dbc0b82e

--- src/HomeWorkApp.java
+++ src/HomeWorkApp.java
@@ -1,150 +1,89 @@
-import java.util.Arrays;
-
 public class HomeWorkApp {
     public static void main(String[] args){
+        printThreeWords();
+        checkSumSign();
+        printColor();
+        compareNumbers();
+        System.out.println(sumNumbers(9, 8));
+        isPositiveNumbers(-5);
+        System.out.println(isNegativeInteger(-10));
+        stringInteger(5, "Привет");
+        System.out.println(leapYear(2000));
 
-        //Задание 1.
-        homeWork1();
-
-        //Задание 2.
-        homeWork2();
-
-        //Задание 3.
-        homeWork3();
-
-        //Задание 4.
-        homeWork4();
-
-        //Задание 5.
-        System.out.println(Arrays.toString(homeWork5(5,6)));
-
-        //Задание 6.
-        homeWork6();
-
-        //Задание 7.
-        System.out.println(homeWork7(new int[]{4,4,2,3,4,9} ));
-
-        //Задание 8.
-        homeWork8(new int[]{1, 2, 3, 4, 5},-2);
     }
 
-    private static void homeWork1() {
-        int[] arreys = {0, 1, 1, 0, 0, 0, 1, 0, 0, 1};
-        for(int i=0;i<arreys.length;i++){
-            if (arreys[i] == 0){
-                arreys[i] = 1;
-            }
-            else if(arreys[i] == 1){
-                arreys[i] = 0;
-            }
-        }
-        System.out.println(Arrays.toString(arreys));
+    public static void printThreeWords(){
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
     }
 
-    private static void homeWork2() {
-        int[] arreys2 = new int[100];
-        for(int i = 0; i<100;i++){
-            arreys2 [i] = (i+1);
+    public static void checkSumSign(){
+        int a = 10;
+        int b = 20;
+        if(a+b>0){
+            System.out.println("Сумма положительная");
         }
-        System.out.println(Arrays.toString(arreys2));
-    }
-
-    private static void homeWork3() {
-        int[] arreys3 = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
-        for (int i=0; i<arreys3.length;i++){
-            if(arreys3[i] < 6){
-                arreys3[i] = (arreys3[i]*6);
-            }
-        }
-        System.out.println(Arrays.toString(arreys3));
-    }
-
-    private static void homeWork4() {
-        int[][] arr = new int [10][10];
-        for(int i=0;i<arr.length;i++){
-            int j;
-            for (j = 0;j<arr.length;j++){
-                if(i == j){
-                    arr[i][j] = 1;
-                }
-            }
-            for (j=0;j<arr.length;j++){
-                if((arr.length-i-1) == j){
-                    arr[i][j] = 1;
-                }
-            }
-        }
-        for (int i=0;i<arr.length;i++){
-            System.out.println(Arrays.toString(arr[i]));
+        else {
+            System.out.println("Сумма отрицательная");
         }
     }
 
-    public static int[] homeWork5(int len, int initialValue) {
-        int[] arr = new int[len];
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = initialValue;
+    public static void printColor(){
+        int value = 97;
+
+        if (value<=0){
+            System.out.println("Красный");
         }
-        return arr;
+        else if (value>0||value<=100){
+            System.out.println("Желтый");
+        }
+        else if (value>100){
+            System.out.println("Зеленый");
+        }
     }
 
-    private static void homeWork6() {
-        int[] arreys6 = {10, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
-        int max = arreys6[0];
-        for (int i =0;i<arreys6.length;i++){
-            if(arreys6[i]>max){
-                max = arreys6[i];
-            }
+    public static void compareNumbers(){
+        int a = 35;
+        int b = 29;
+        if(a>=b){
+            System.out.println("a>=b");
         }
-        System.out.println(max);
-
-        int min = arreys6[0];
-        for (int i =0;i<arreys6.length;i++){
-            if(arreys6[i]<min){
-                min = arreys6[i];
-            }
+        else{
+            System.out.println("a<b");
         }
-        System.out.println(min);
     }
 
-    public static boolean homeWork7(int[] arreys7){
-        for(int i=0; i<arreys7.length-2;i++){
-            int summ1 =0;
-            for (int j=0; j<=i;j++){
-                summ1 = summ1 + arreys7[j];
-            }
-
-            int summ2 =0;
-            for (int k = i+1;k<arreys7.length;k++){
-                summ2 = summ2 + arreys7[k];
-            }
-            if(summ1 == summ2){
-                return true;
-            }
-        }
-        return false;
+    public static boolean sumNumbers(int a, int b){
+        return a+b>=10 && a+b<=20;
     }
 
-    //Задание 8.
-    public static void homeWork8(int[] arreys8, int n){
-        //{4,3,8,6}
-        if(n>0) {
-            for (int k = 0; k < n; k++) {
-                int j = arreys8[0];
-                for (int i = 0; i < arreys8.length - 1; i++) {
-                    arreys8[i] = arreys8[i + 1];
-                }
-                arreys8[arreys8.length - 1] = j;
-            }
+    public static void isPositiveNumbers(int a){
+        if(a>=0){
+            System.out.println("Число положительное");
         }
-        if(n<0){
-            for (int k = 0; k < (-n); k++) {
-                int j = arreys8[arreys8.length-1];
-                for (int i=arreys8.length-1; i>0; i--) {
-                    arreys8[i] = arreys8[i-1];
-                }
-                arreys8[0] = j;
-            }
+        else {
+            System.out.println("Число отрицательное");
         }
-        System.out.println(Arrays.toString(arreys8));
+    }
+
+    public static boolean isNegativeInteger(int a){
+        return a<0;
+    }
+
+    public static void stringInteger(int a, String s) {
+        for (int i = 0; i < a; i++) {
+            System.out.println(s);
+        }
+    }
+
+    public static boolean leapYear(int a){
+        if (a % 400 == 0) {
+            return true;
+        }
+        if (a % 100 == 0) {
+            return false;
+        }
+        return a % 4 == 0;
     }
 }
